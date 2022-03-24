49cf27e1b231e370c3d1c2720dd5565a2b40bf41
Linyun <rich.xia@gmail.com>
2022-03-24 15:13:00 +0800

    完成第一个welcome页面2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  root :to => "welcome#index"
 end

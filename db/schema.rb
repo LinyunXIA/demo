3a68367813d0dc9a638baca18eb9f1997396170d
Linyun <rich.xia@gmail.com>
2022-03-25 14:56:20 +0800

    Controller & 页面的建立

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_071913) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_25_012849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "is_public"
+    t.integer "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
